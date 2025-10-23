466ca87f4141537dd938896bbd79da961a863065
medalimami <dalymami741@outlook.com>
2025-10-23 12:44:10 -0400

    installed requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical-ChatBot",
+    version="0.1.0",
+    author="Medali Mami",
+    author_email="dalymami741@outlook.com",
+    packages=find_packages(),
+    install_requires=[]
+)
